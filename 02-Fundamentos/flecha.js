a30672f03dc015c35b2ccdada3c9ce28091f4529
alicia-granados <aliciagranados35@gmail.com>
2023-05-26 15:00:22 -0600

    ✨ feat:Funciones de flecha

---
+++ 02-Fundamentos/flecha.js
@@ -0,0 +1,11 @@
+const sumar = (a, b) => {
+    return a + b; 
+}
+
+const sumar2 = (a, b) =>  a + b; 
+
+console.log(sumar(10,5));
+console.log(sumar2(8,4));
+
+const saludar = () => 'Hola Mundo';
+console.log(saludar())
