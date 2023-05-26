e25b608d1fc542f43269ca18b8968f7c4d6f473f
alicia-granados <aliciagranados35@gmail.com>
2023-05-26 14:36:33 -0600

    ✨ feat:Template Strings

---
+++ 02-Fundamentos/template-strings.js
@@ -0,0 +1,13 @@
+const nombre = 'Deadpool';
+const real = 'Wade Winston';
+
+const normal = nombre + ' ' + real
+console.log(normal);
+
+const template = `${nombre} ${real} `
+console.log(template);
+
+const html = `
+<h1> Hola </h1>
+`;
+console.log(html)
