37710476df12db76bc9337ca5b584e491bfa82c6
alicia-granados <aliciagranados35@gmail.com>
2023-05-26 14:27:54 -0600

    ✨ feat:Const vs Let vs Var

---
+++ 02-Fundamentos/const-var-let.js
@@ -0,0 +1,19 @@
+var nombre = 'Andrea';
+if ( true ) {
+    var nombre = 'Ale';
+}
+console.log(nombre);
+
+
+let nombre2 = 'Andres';
+
+if ( true ) {
+    nombre2 = 'Ale';
+}
+console.log(nombre2);
+
+const nombre3 = 'Andres';
+if ( true ) {
+    const nombre3 = 'Ale';
+}
+console.log(nombre2);
