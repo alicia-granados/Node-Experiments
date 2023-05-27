877c358381787cba925b40ef271b2e676788336c
alicia-granados <aliciagranados35@gmail.com>
2023-05-27 14:37:08 -0600

    ✨ feat:yargs

--- 03-BasesNode/app.js
+++ 03-BasesNode/app.js
@@ -1,6 +1,10 @@
 const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('yargs').argv
 
 console.clear();
+
+console.log(argv)
+console.log('base: yargs', argv.base)
 
 const base = 3;
 crearArchivo(base)
