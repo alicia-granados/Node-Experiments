1e18462663e76642c472dec3422774543cc58d29
alicia-granados <aliciagranados35@gmail.com>
2023-05-27 17:20:20 -0600

    ✨ feat:colores de la consola

--- 03-BasesNode/app.js
+++ 03-BasesNode/app.js
@@ -1,6 +1,7 @@
 const {crearArchivo} = require('./helpers/multiplicar');
 const argv = require('./config/yargs');
-
+const colors = require('colors');
+ 
 console.clear();
 
 crearArchivo(argv.b, argv.l )
--- 03-BasesNode/helpers/multiplicar.js
+++ 03-BasesNode/helpers/multiplicar.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
+const colors = require('colors');
 
 const crearArchivo = async( base = 5 , listar = false) => {
     try{
         let salida = ''; 
         for (let i = 1; i <= 10; i++) {
-            salida += `${base} x ${i} = ${base *  i }\n`;
+            salida += `${base} ${'x'.yellow} ${i} ${'='.blue} ${base *  i }\n`.underline.red;
         }
 
         if (listar){
-            console.log('===============')
-            console.log(`  TABLA DEL:` , base)
-            console.log('===============')
+            console.log('==============='.green)
+            console.log(`  TABLA DEL:`.red ,colors.cyan(base) )
+            console.log('==============='.green)
             console.log(salida);
         }
             
         fs.writeFileSync(`tabla-${base}.txt`, salida);
-        return `tabla-${base}.txt creado` 
+        return `tabla-${base}.txt creado`.rainbow; 
          
     }catch (error) {
         throw error
