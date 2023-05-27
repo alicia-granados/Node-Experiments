d2ce50ff6fba40d797f0528383f82d75198bdd2a
alicia-granados <aliciagranados35@gmail.com>
2023-05-27 12:44:11 -0600

    ✨ feat:Requerir  paquetes -require

--- 03-BasesNode/app.js
+++ 03-BasesNode/app.js
@@ -1,12 +1,19 @@
+const fs = require('fs');
+
 console.clear();
 console.log('===============')
 console.log('==TABLA DEL 4==')
 console.log('===============')
-const multiplicacion = (num) =>{
-    operacion = num * 5
-    return operacion
-} 
 
+const base = 5;
+let salida = ''; 
 for (let i = 1; i <= 10; i++) {
-    console.log(`5 x ${i} =` ,multiplicacion(i));
+    salida += `${base} x ${i} = ${base *  i }\n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) =>{
+    if( err) throw err;
+    console.log(`tabla-${base}.txt creado`);
+})
