b627731760768be2a19899182ff20328c500549c
alicia-granados <aliciagranados35@gmail.com>
2023-05-27 17:52:07 -0600

    ✨ feat:README

---
+++ 03-BasesNode/README.md
@@ -0,0 +1,10 @@
+# NOTAS:
+```
+Options:
+      --help     Show help                                             [boolean]
+      --version  Show version number                                   [boolean]
+  -b, --base     Toma la base de la tabla de multiplicar     [number] [required]
+  -l, --listar   Muestra la tabla en consola          [boolean] [default: false]
+  -h, --hasta    Indica hasta que numero llegara la multiplicacion
+                                                             [number] [required]
+```
--- 03-BasesNode/config/yargs.js
+++ 03-BasesNode/config/yargs.js
@@ -1,17 +1,22 @@
 const argv = require('yargs')
-    .options (
+    .option (
         'b', {
             alias: 'base',
             type : 'number',
             demandOption: true,
             describe: 'Toma la base de la tabla de multiplicar'
-        },
+        }
+    )
+    .option(
         'l',{
             alias: 'listar',
             type: 'boolean',
             default: false,
             describe: 'Muestra la tabla en consola'
-        },
+        }
+    )
+    .option(
+
         'h',{
             alias: 'hasta',
             type: 'number',
