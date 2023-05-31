7030cb83d5945e55bb49059a170b4ce4b29c4a9c
alicia-granados <aliciagranados35@gmail.com>
2023-05-31 12:16:01 -0600

    ✨ feat:Realizar peticiones HTTP desde Node

--- 05-climaApp/index.js
+++ 05-climaApp/index.js
@@ -4,20 +4,35 @@ import {
     leerInput,
     pausa
 } from './helpers/inquirer.js';
+import { Busquedas } from './models/busquedas.js';
 
 const main = async() =>{
 
     let opt ;
+    const busquedas = new Busquedas();
 
     do {
         //imprimier el menu
         opt = await inquirerMenu();
     
         switch (opt) {
-          case '1':
-            //crear tarea
-            const desc = await leerInput('Descripcion: ');
-            console.log(desc)
+          case 1:
+            //Mostrar mensaje
+            const lugar = await leerInput('Ciudad: ');
+            await busquedas.ciudad(lugar);
+            //Buscar los lugares
+            //seleccionar el lugar
+            //clima
+            //mostrar rresultados
+            
+            console.log('\nInformación de la ciudad\n'.green);
+            console.log('Ciudad: ', );
+            console.log('Lat: ', );
+            console.log('Lng: ', );
+            console.log('Temperatura: ', );
+            console.log('Mínima: ', );
+            console.log('Maxíma: ', );
+
             break;
           case '2':
           break;
---
+++ 05-climaApp/models/busquedas.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+class Busquedas{
+    historial = ['mexico','san jose'];
+
+    constructor(){
+        //leer db si existe
+    }
+
+    async ciudad(lugar= ''){
+        try{
+            //Petición HTTP
+            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            console.log( resp.data);
+        return []; // retornar los lugares
+        }catch (error) {
+            return [];
+        }
+
+    }
+
+}
+export {Busquedas}
