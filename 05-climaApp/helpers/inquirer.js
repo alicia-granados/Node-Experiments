ea2275569964c02f5e780a3d731c8c93300e7a24
alicia-granados <aliciagranados35@gmail.com>
2023-05-31 11:21:11 -0600

    ✨ feat:Menú de la aplicación

--- 05-climaApp/helpers/inquirer.js
+++ 05-climaApp/helpers/inquirer.js
@@ -9,31 +9,15 @@ const menuOpts = [
     message: 'Seleccione una opción',
     choices: [
         {
-            value: '1',
-            name:  `${'1.'.green}Crear tarea`,
+            value: 1,
+            name:  `${'1.'.green} Buscar Ciudad`,
         },
         {
-            value: '2',
-            name: `${'2.'.green}Listar tarea`,
+            value: 2,
+            name: `${'2.'.green} Historial`,
         },
         {
-            value: '3',
-            name: `${'3.'.green}Listar tareas completadas`,
-        },
-        {
-            value: '4',
-            name: `${'4.'.green}Listar tareas pendientes`,
-        },
-        {
-            value: '5',
-            name: `${'5.'.green}Completar tarea(s)`,
-        },
-        {
-            value: '6',
-            name: `${'6.'.green}Borrar tarea`,
-        },
-        {
-            value: '0',
+            value: 0,
             name: `${'0.'.green} Salir \n`,
         },
     ],
--- 05-climaApp/index.js
+++ 05-climaApp/index.js
@@ -1,12 +1,29 @@
 import colors from 'colors';
 import { 
-    leerInput
+    inquirerMenu,
+    leerInput,
+    pausa
 } from './helpers/inquirer.js';
 
 const main = async() =>{
 
-    const texto = await leerInput('Hola  ');
-    console.log(texto);
+    let opt ;
+
+    do {
+        //imprimier el menu
+        opt = await inquirerMenu();
+    
+        switch (opt) {
+          case '1':
+            //crear tarea
+            const desc = await leerInput('Descripcion: ');
+            console.log(desc)
+            break;
+          case '2':
+          break;
+        }
+        await pausa();
+      } while (opt !== 0);
     
 }
 
