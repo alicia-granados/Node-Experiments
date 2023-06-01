3f2a1571a1cb1d4775f29390d2b84981568b539d
alicia-granados <aliciagranados35@gmail.com>
2023-05-31 20:27:08 -0600

    ✨ feat:Listar los países de forma interactiva

--- 05-climaApp/index.js
+++ 05-climaApp/index.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import { 
     inquirerMenu,
     leerInput,
+    listarLugares,
     pausa
 } from './helpers/inquirer.js';
 import { Busquedas } from './models/busquedas.js';
@@ -21,17 +22,24 @@ const main = async() =>{
         switch (opt) {
           case 1:
             //Mostrar mensaje
-            const lugar = await leerInput('Ciudad: ');
-            await busquedas.ciudad(lugar);
+            const termino = await leerInput('Ciudad: ');
+    
             //Buscar los lugares
+            const lugares = await busquedas.ciudad(termino);
+
             //seleccionar el lugar
+            const id = await listarLugares(lugares);
+            const lugareSel = lugares.find(l => l.id === id);
+            console.log(lugareSel);
+
             //clima
+
             //mostrar rresultados
             
             console.log('\nInformación de la ciudad\n'.green);
-            console.log('Ciudad: ', );
-            console.log('Lat: ', );
-            console.log('Lng: ', );
+            console.log('Ciudad: ', lugareSel.nombre);
+            console.log('Lat: ', lugareSel.lat );
+            console.log('Lng: ', lugareSel.lng);
             console.log('Temperatura: ', );
             console.log('Mínima: ', );
             console.log('Maxíma: ', );
--- 05-climaApp/models/busquedas.js
+++ 05-climaApp/models/busquedas.js
@@ -24,8 +24,14 @@ class Busquedas{
             })
 
             const resp = await intance.get();
-            console.log( resp.data);
-            return []; // retornar los lugares
+
+            return  resp.data.features.map(lugar =>({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+            }));
+            
         } catch ( error) {
             return [];
         }
