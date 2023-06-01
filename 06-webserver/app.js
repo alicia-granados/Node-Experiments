28ec8db20054119b4b007308f5e72f30d4ea05be
alicia-granados <aliciacontact6@gmail.com>
2023-06-01 14:58:08 -0600

    ✨ feat:Servir un sitio web completo

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -5,9 +5,14 @@ port = 8080;
 //servir contenido estatico 
 app.use(express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectiva ruta')
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
 });
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
+});
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html')
 });
