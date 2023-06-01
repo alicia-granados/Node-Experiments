061ea9414f28e5ef082624f4810e937c68707152
alicia-granados <aliciacontact6@gmail.com>
2023-06-01 15:13:37 -0600

    ✨ feat:Handlebars

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 port = 8080;
 
+//TODO: require('hbs')
+app.set('view engine', 'hbs');
+
 //servir contenido estatico 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
 
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html')
