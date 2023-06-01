3666ba4103343532dac3fee6a823163d9bb5def1
alicia-granados <aliciacontact6@gmail.com>
2023-06-01 12:09:03 -0600

    ✨ feat:Request y Response

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 http.createServer(( req, res) =>{
-    res.write('Hola mundo')
+    console.log(req);
+
+    //res.writeHead(200, {'Content-Type': 'text/plain'});
+    /* res.writeHead(200, {'Content-Type': 'application/json'});
+    const persona={
+        id:1,
+        nombre:'Fer'
+    }
+
+    res.write(JSON.stringify(persona))*/
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.write('1,nombre\n');
+    res.write('2,adsa\n');
+
     res.end()
 })
 .listen(8080);
