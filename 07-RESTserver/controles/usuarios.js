24f15f27a52eae961abf814bf52449492b73ceba
alicia-granados <aliciacontact6@gmail.com>
2023-06-02 19:41:54 -0600

    ✨ feat:Parámetros de segmento y query

--- 07-RESTserver/controles/usuarios.js
+++ 07-RESTserver/controles/usuarios.js
@@ -1,15 +1,24 @@
-const {response} = require('express')
+const {response  , request} = require('express')
 
 
-const usuariosGet = (req, res = response ) => {
+const usuariosGet = (req = params, res = response ) => {
+    const  {q, nombre = 'No name',apikey , page = 1 , limit} = req.query;
+
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q, 
+        nombre,
+        apikey,
+        page,
+        limit
     })
 }
 
 const  usuariosPut = (req, res = response ) => {
+    const id = req.params.id
     res.json({
-        msg: 'put API - controlador'
+        msg: 'put API - controlador',
+        id
     })
 }
 
