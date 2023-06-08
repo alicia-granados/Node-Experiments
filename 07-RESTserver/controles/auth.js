370a90fa78151b898cee355a42596202d56f82ec
alicia-granados <aliciacontact6@gmail.com>
2023-06-08 11:02:23 -0600

    ✨ feat:Login usuario

--- 07-RESTserver/controles/auth.js
+++ 07-RESTserver/controles/auth.js
@@ -1,9 +1,52 @@
-const {response } = require('express')
+const {response } = require('express');
+const  bycryptjs = require('bcryptjs');
+const Usuario  = require('../models/usuario');
 
-const login = (req, res = rsponse) => {
-    res.json ({
-        msg : 'LOGIN ok'
-    })
+const login = async (req, res = response) => {
+
+    const {correo, password} = req.body;
+
+    try{
+        //verificar si el email existe
+        const usuario = await Usuario.findOne({correo});
+
+        if ( !usuario ){
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+        //si el usurio esta activo.
+        if ( !usuario.estado  ){
+            return res.status(400).json({
+                msg: 'Usuario / no son correctos - estado : false'
+            })
+        }
+         
+        //verificar la contraseña
+        const validPassword = bycryptjs.compareSync(password, usuario.password);
+
+        if( !validPassword ){
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        //generar el JWT
+
+        res.json ({
+            msg : 'LOGIN ok'
+        })
+
+    } catch (error){
+
+        console.log(error)
+        return res.status(500).json({
+            msg: 'Hable con el admin'
+        })
+
+    }
+
+
 }
 
 module.exports ={
