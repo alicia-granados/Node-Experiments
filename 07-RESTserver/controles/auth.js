48ede96df199c08a4c0f44ee58029dd2a5476ae4
alicia-granados <aliciacontact6@gmail.com>
2023-06-10 21:44:45 -0600

    ✨ feat: crear usuario personalizado con las credenciales de google

--- 07-RESTserver/controles/auth.js
+++ 07-RESTserver/controles/auth.js
@@ -57,21 +57,47 @@ const googleSignIn = async ( req, res = response) =>{
     const {id_token} = req.body;
 
     try {
-        const {nombre, img, correo} = await googleVerify(id_token);
-
+        const { correo, nombre, img } = await googleVerify( id_token );
+ 
+        let usuario = await Usuario.findOne({ correo });
+ 
+        if ( !usuario ) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+ 
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+ 
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+ 
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
         res.json({
-            msg: 'Todo bien',
-            id_token
-        })
-
+            usuario,
+            token
+        });
+        
     } catch (error) {
-        json.status(400).json({
-            ok:false,
-            msg: 'el token no se pudo verificar'
+ 
+        res.status(400).json({
+            msg: 'Token de Google no es válido',
+            error
         })
     }
 
-  
 }
 
 module.exports ={
