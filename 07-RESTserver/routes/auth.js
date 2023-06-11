7dfb5df1c939cf2bf5dfd91ac3915caffb7832f7
alicia-granados <aliciacontact6@gmail.com>
2023-06-10 20:35:13 -0600

    ✨ feat:ruta para manejar autenticación de google

--- 07-RESTserver/routes/auth.js
+++ 07-RESTserver/routes/auth.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const {check } = require('express-validator');
-const { login } = require('../controles/auth');
+const { login, googleSignIn } = require('../controles/auth');
 const { validarCampos } = require('../middlewares/validar_campos');
 
 const router = Router();
@@ -10,5 +10,10 @@ router.post('/login',[
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ], login);  
+
+router.post('/google',[  
+    check('id_token', ' ID Token de google es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);  
 
 module.exports= router;
