9403a79af4328e0ee79b9ad5fed09f83b3ac78e7
alicia-granados <aliciacontact6@gmail.com>
2023-06-26 17:06:14 -0600

    ✨ feat:validar extension

--- 07-RESTserver/controles/uploads.js
+++ 07-RESTserver/controles/uploads.js
@@ -10,6 +10,18 @@ const cargarArchivo = ( req, res = response) =>{
 
     const { archivo } = req.files;
 
+    const nombreCortado  = archivo.name.split('.');
+    const extension= nombreCortado[ nombreCortado.length -1 ];
+
+    //validar la  extension
+    const extensionValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if( !extensionValidas.includes(extension)){
+        return res.status(400).json({
+            msg: `La extension ${extension} no es permitida, ${extensionValidas}`
+        })
+    }
+
     const uploadPath = path.join(__dirname , '../uploads/', archivo.name);
 
     archivo.mv(uploadPath, (err) =>  {
