14dc58bcb33b4d18ed07fef27e09a71a68692359
alicia-granados <aliciacontact6@gmail.com>
2023-06-27 13:26:34 -0600

    ✨ feat:mensajes de conexion y desconexion - cliente

--- 08-wenSocket-server/public/js/socket-client.js
+++ 08-wenSocket-server/public/js/socket-client.js
@@ -1,1 +1,19 @@
+//referencias del html
+const lblonline = document.querySelector('#lblonline')
+const lbloffline = document.querySelector('#lbloffline')
+
 const socket = io();
+
+//n escuchar eventos
+socket.on('connect', () =>{
+    console.log('Conectado');
+
+    lbloffline.style.display = 'none';
+    lblonline.style.display ='';
+});
+
+socket.on('disconnect', () =>{
+    console.log('desconectado del servidor')
+    lblonline.style.display = 'none';
+    lbloffline.style.display ='';
+});
