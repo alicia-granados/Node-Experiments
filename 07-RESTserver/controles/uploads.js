eb3c06aa098305c0037012774507737027f19437
alicia-granados <aliciacontact6@gmail.com>
2023-06-26 22:09:13 -0600

    ✨ feat:actualizar imagen de usuario y productos

--- 07-RESTserver/controles/uploads.js
+++ 07-RESTserver/controles/uploads.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
+const { Usuario, Producto } = require("../models");
 
 const cargarArchivo = async ( req, res = response) =>{
 
@@ -29,7 +30,38 @@ const cargarArchivo = async ( req, res = response) =>{
 
 const actualizarImagen =async (req, res = response) =>{
     const {id, coleccion } = req.params;
-    res.json({id, coleccion})
+
+    let modelo ;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if( !modelo ){
+                return res.status(400).json({
+                    msg: `NO existe un usuario con el id ${id}`
+                })
+            }
+            
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if( !modelo ){
+                return res.status(400).json({
+                    msg: `NO existe un producto con el id ${id}`
+                })
+            }
+            
+            break;
+
+        default:
+            res.status(500).json({msg:'se me olvido validar esto'});
+    }
+
+    const nombre = await subirArchivo(req.files,undefined,coleccion);
+    modelo.img = nombre; 
+    await modelo.save();
+
+    res.json(modelo)
 }
 
 module.exports ={
--- 07-RESTserver/routes/uploads.js
+++ 07-RESTserver/routes/uploads.js
@@ -7,6 +7,7 @@ const { coleccionesPermitidas } = require('../helpers');
 const router = Router();
 
 router.post('/', cargarArchivo);  
+
 router.put('/:coleccion/:id', [
     check('id', 'El id debe de ser de mongo').isMongoId(),
     check('coleccion').custom(c =>coleccionesPermitidas(c, ['usuarios', 'productos'])),
