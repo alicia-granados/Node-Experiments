36209fa34346f581037688fdcd458229bbb2f280
alicia-granados <aliciacontact6@gmail.com>
2023-06-29 11:05:05 -0600

    ✨ feat:configurar Socket.io

--- 10-socket-chat/models/server.js
+++ 10-socket-chat/models/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-
+const {createServer} = require('http');
 const { dbConnection } = require('../database/config');
+const { Socket } = require('socket.io');
+const { socketControler } = require('../sockets/socketController');
 
 class Server {
 
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+        this.server = createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         this.paths = {
             auth:       '/api/auth',
@@ -28,6 +32,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        //Sockets
+        this.sockets();
     }
 
     async conectarDB() {
@@ -65,16 +72,17 @@ class Server {
         this.app.use( this.paths.uploads, require('../routes/uploads'));
         
     }
+    sockets(){
+        this.io.on('connection', socketControler)
+    }
+
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
 
 }
-
-
-
 
 module.exports = Server;
---
+++ 10-socket-chat/sockets/socketController.js
@@ -0,0 +1,9 @@
+const { Socket } = require("socket.io")
+
+const socketControler = (socket ) => {
+    console.log('cliente conectado', socket.id)
+}
+
+module.exports = {
+    socketControler
+}
