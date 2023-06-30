9195a4f84555c898ffd854bf92120cb3a3ac3608
alicia-granados <aliciacontact6@gmail.com>
2023-06-29 18:22:02 -0600

    ✨ feat:HTML Y JS que usaremos

--- 10-socket-chat/public/js/chat.js
+++ 10-socket-chat/public/js/chat.js
@@ -6,6 +6,12 @@ const url = ( window.location.hostname.includes('localhost'))
 let usuario = null;
 let socket = null;
 
+//Referencias html
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const ulUsuarios = document.querySelector('#ulUsuarios');
+const ulMensaje = document.querySelector('#ulMensaje');
+
 // validar el token del localstorage
 const  validarJWT  = async () => {
     const token = localStorage.getItem('token') || '';
@@ -29,12 +35,31 @@ const  validarJWT  = async () => {
 
 const conectarSocket = async() =>{
 
-    const socket = io({
+    socket = io({
         'extraHeaders': {
             'x-token': localStorage.getItem('token')
         }
     });
 
+    socket.on('connect', () => {
+        console.log('Sockets online')
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Sockets offline')
+    });
+
+    socket.on('recibir-mensajes' , () => {
+        //TODO: 
+    });
+
+    socket.on('usuarios-activos' , () => {
+        //TODO: 
+    });
+
+    socket.on('mensaje-privado' , () => {
+        //TODO: 
+    })
 }
 
 const main = async () =>{
