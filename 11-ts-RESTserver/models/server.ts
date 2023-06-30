9fe559a9028d655dde41d1c5ec3a991d0f68e38b
alicia-granados <aliciacontact6@gmail.com>
2023-06-30 12:08:01 -0600

    ✨ feat:rutas de la aplicacion

--- 11-ts-RESTserver/models/server.ts
+++ 11-ts-RESTserver/models/server.ts
@@ -1,14 +1,23 @@
 import express,{Application} from 'express';
+import userRoutes from '../routes/usuario';
 
 class Server {
     private app: Application;
     private port: string;
+    private  apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8080';
-    }
 
+        //definir mis rutas
+        this.routes();
+    }
+    routes(){
+        this.app.use( this.apiPaths.usuarios, userRoutes)
+    }
     listen(){
         this.app.listen(this.port, () =>{
             console.log('servidor corriendo en el puerto' + this.port)
